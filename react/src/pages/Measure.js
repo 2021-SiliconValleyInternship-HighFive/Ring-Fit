396a298c9ccaadc6fe04eed65edccaaf67160007
jueun <rrnarg@gmail.com>
2021-07-17 00:18:21 +0900

    NEW: react 기본 페이지(Home - Result) 생성

---
+++ react/src/pages/Measure.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Measure() {
+    return(
+        <div></div>
+    );
+}
+
+export default Measure;
---
+++ react/src/pages/Upload.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Upload() {
+    return(
+        <div></div>
+    );
+}
+
+export default Upload;
