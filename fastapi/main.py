ddafe8e1352356809300eee3d7264f8373c05035
youeun <dbslsy121@gmail.com>
2021-07-17 18:57:36 +0900

    NEW: CORSMiddleware 추가

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-# from fastapi.encoders import jsonable_encoder
 
+# Backend origin
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+# CORSMiddleware 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+'''
+APIs
+'''
 class Value(BaseModel):
     x: int
     y: int
