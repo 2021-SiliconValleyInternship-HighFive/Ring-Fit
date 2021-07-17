81bb5ca5a70b793dd344371cb67d041687f05c92
youeun <dbslsy121@gmail.com>
2021-07-17 18:18:48 +0900

    NEW: 결과값 반환 API

--- fastapi/main.py
+++ fastapi/main.py
@@ -6,6 +6,10 @@ class Value(BaseModel):
     x: int
     y: int
 
+class Size(BaseModel):
+    perimeter: int
+    size: int
+
 app = FastAPI()
 
 # x, y값 API
@@ -13,7 +17,12 @@ app = FastAPI()
 def find_value(value: Value):
     return value
 
-# resize 된 이미지 파일 API
+# resized 이미지 파일 API
 @app.post('/api/resized-file')
 async def send_resized_file(file: UploadFile = File(...)):
     return {"filename" : file.filename} 
+
+# 결과값 API
+@app.post('/api/result')
+async def get_result(result: Size):
+    return result
