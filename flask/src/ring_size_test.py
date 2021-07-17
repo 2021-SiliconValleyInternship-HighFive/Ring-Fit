bfda28073fb61bfdd2f24cc054286d25fc47794b
leejeongeun <you@example.com>
2021-07-17 22:21:54 +0900

    NEW: 길이측정

---
+++ flask/src/ring_size_test.py
@@ -0,0 +1,70 @@
+from scipy.spatial.distance import euclidean
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import imutils
+import cv2
+
+# 이미지 배열을 보여주는 함수(중간 결과)
+def show_images(images):
+	for i, img in enumerate(images):
+		cv2.imshow("image_" + str(i), img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+img_path = "images/example_02.jpg"
+
+# 이미지 읽기 및 전처리
+image = cv2.imread(img_path)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (9, 9), 0)
+
+edged = cv2.Canny(blur, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+#show_images([blur, edged])
+
+# 등고선 찾기
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# 가장 왼쪽의 윤곽선이 참조 객체이므로 윤곽선을 왼쪽에서 오른쪽으로 정렬합니다.
+(cnts, _) = contours.sort_contours(cnts)
+
+# 충분히 크지 않은 윤곽선 제거
+cnts = [x for x in cnts if cv2.contourArea(x) > 100]
+
+#cv2.drawContours(image, cnts, -1, (0,255,0), 3)
+
+# 참조 객체 치수
+# 참고로 저는 2cm x 2cm 정사각형을 사용했습니다.
+ref_object = cnts[0]
+box = cv2.minAreaRect(ref_object)
+box = cv2.boxPoints(box)
+box = np.array(box, dtype="int")
+box = perspective.order_points(box)
+(tl, tr, br, bl) = box
+dist_in_pixel = euclidean(tl, tr)
+dist_in_cm = 2
+pixel_per_cm = dist_in_pixel/dist_in_cm
+
+# 나머지 윤곽 그리기
+for cnt in cnts:
+	box = cv2.minAreaRect(cnt)
+	box = cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
+	box = perspective.order_points(box)
+	(tl, tr, br, bl) = box
+	cv2.drawContours(image, [box.astype("int")], -1, (0, 0, 255), 2)
+	mid_pt_horizontal = (tl[0] + int(abs(tr[0] - tl[0])/2), tl[1] + int(abs(tr[1] - tl[1])/2))
+	mid_pt_verticle = (tr[0] + int(abs(tr[0] - br[0])/2), tr[1] + int(abs(tr[1] - br[1])/2))
+	wid = euclidean(tl, tr)/pixel_per_cm
+	ht = euclidean(tr, br)/pixel_per_cm
+	cv2.putText(image, "{:.1f}cm".format(wid), (int(mid_pt_horizontal[0] - 15), int(mid_pt_horizontal[1] - 10)), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+	cv2.putText(image, "{:.1f}cm".format(ht), (int(mid_pt_verticle[0] + 10), int(mid_pt_verticle[1])), 
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
+
+show_images([image])
