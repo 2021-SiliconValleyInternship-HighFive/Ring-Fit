5185bc982ac4f79193ff59f1d40acee6b417eaf6
jueun <rrnarg@gmail.com>
2021-07-17 15:15:02 +0900

    NEW: (react) npm install 'react-image-file-resizer'

--- react/src/pages/Upload.js
+++ react/src/pages/Upload.js
@@ -2,8 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Upload() {
+
   return (
     <div>
+       <input
+        type="file"
+        accept="image/*"
+        id="upload-file"
+      />
+
+      {/*route link*/}
       <Link to="/measure">
         <button>select</button>
       </Link>
