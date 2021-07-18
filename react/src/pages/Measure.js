7f3ddc47a5930f51e3d4fd681a620acd11d74f65
jueun <rrnarg@gmail.com>
2021-07-18 22:38:33 +0900

    NEW: 드래그 기능

--- react/src/pages/Measure.js
+++ react/src/pages/Measure.js
@@ -6,14 +6,22 @@ function Measure() {
   const imgUrl = location.state.imgUrl;
 
   const [coord, setCoord] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ left: 100, top: 100 });
 
   /* 이미지 좌표 추출 */
   const touchHandler = (e) => {
-    const bcr = e.getBoundingClientRect();
+    const bcr = document.getElementById("image").getBoundingClientRect();
     const x = e.targetTouches[0].pageX - bcr.x;
     const y = e.targetTouches[0].pageY - bcr.y;
-    console.log(e.targetTouches[0].pageX, "bcr: ", bcr.x);
+    console.log(e.targetTouches[0].pageX,  "bcr: ", bcr.x);
     setCoord({ x: x, y: y });
+    if(y>0) {
+      console.log("y>0");
+    }
+    setPosition({
+      left: e.targetTouches[0].pageX - 25,
+      top: e.targetTouches[0].pageY - 10,
+    });
   };
 
   /* offsetX,Y  확인용 테스트 함수 */
@@ -27,8 +35,14 @@ function Measure() {
   const imgStyle = {
     width: "90%",
     height: "90%",
+    cursor: "pointer",
   };
 
+  const dragStyle = {
+    position: "absolute",
+    top: position.top,
+    left: position.left,
+  };
 
   return (
     <div>
@@ -36,16 +50,15 @@ function Measure() {
       <h1>
         coord x: {coord.x} y: {coord.y}
       </h1>{" "}
-
-      <img
-        id="image"
-        src={imgUrl}
-        style={imgStyle}
+      <div
         onClick={onClick}
         onTouchStart={touchHandler}
         onTouchMove={touchHandler}
-      />
-      
+      >
+      <img  src="https://placeimg.com/50/20/any" style={dragStyle} />
+      </div>
+      <img id="image" src={imgUrl} style={imgStyle} />
+
       <footer>
         <Link to="/upload">취소</Link>
         <Link to="/result">확인</Link>
