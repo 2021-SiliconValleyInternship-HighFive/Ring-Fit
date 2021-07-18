7b3c9df8f381f9cbcd4a672abac776a71b57deb7
leejeongeun <you@example.com>
2021-07-18 23:52:20 +0900

    IMPROVE: move to the root of the project directory

---
+++ temp-pwa/config-overrides.js
@@ -0,0 +1,15 @@
+const { override, addWebpackPlugin } = require('customize-cra');
+const { InjectManifest } = require('workbox-webpack-plugin');
+module.exports = (webpack, ...args) => {
+  // remove GenerateSW plugin
+  webpack.plugins.pop();
+  const overridenConf = override(
+    addWebpackPlugin(
+      new InjectManifest({
+        swSrc: './src/custom-serviceWorker.js',
+        swDest: './service-worker.js',
+      }),
+    ),
+)(webpack, ...args);
+  return overridenConf;
+};
