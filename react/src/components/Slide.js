2d62bfa60254973631178f955eead778bd6b7d07
Ko won jun <kbvv67890@gmail.com>
2021-07-19 02:22:28 +0900

    IMPROVE: intro 페이지 디자인 모바일 개선

--- react/src/components/Slide.js
+++ react/src/components/Slide.js
@@ -20,36 +20,27 @@ render() {
     <Button 
     variant="info"
     style={{fontFamily: 'cookie',textAlign:'center',
-    color: 'white', fontWeight: '900',padding: '1.375rem 7.75rem',backgroundColor:  'rgb(13, 202, 240)',
+    color: 'white', fontWeight: '900',width:'100vw',padding: '1.375rem 7.75rem',backgroundColor:  'rgb(13, 202, 240)',
     borderColor:  'rgb(13, 202, 240)'}} >RING-FIT 사용법💍</Button>{' '}
         <Slider {...settings}>
         <div>
         <img src = "https://placeimg.com/300/300/1"
-            width='375'
-            height='500'
-            style={{paddingRight: '50px',paddingLeft: '50px', paddingTop: '100px', paddingBottom: '100px',borderTopWidth: '0px'}}/>
+            style={{display: 'block', margin: '0px auto',paddingTop: '50px' ,paddingBottom: '50px'}}/>
             <h3 style={{fontFamily: 'cookie',textAlign:'center',color:'rgb(13, 202, 240)'}}>동전과 손을 준비하세요!</h3>
-            
         </div>
         <div>
         <img src = "https://placeimg.com/300/300/2"
-            width='375' 
-            height='500'
-            style={{paddingRight: '50px',paddingLeft: '50px', paddingTop: '100px', paddingBottom: '100px',borderTopWidth: '0px'}}/>
+            style={{display: 'block', margin: '0px auto',paddingTop: '50px' ,paddingBottom: '50px'}}/>
             <h3 style={{fontFamily: 'cookie',textAlign:'center',color:'rgb(13, 202, 240)'}}>동전을 왼쪽에, 손을 오른쪽에 두고 사진을 찍어주세요.</h3>
         </div>
         <div>
         <img src = "https://placeimg.com/300/300/3"
-            width='375' 
-            height='500'
-            style={{paddingRight: '50px',paddingLeft: '50px', paddingTop: '100px', paddingBottom: '100px',borderTopWidth: '0px'}}/>
+            style={{display: 'block', margin: '0px auto',paddingTop: '50px' ,paddingBottom: '50px'}}/>
             <h3 style={{fontFamily: 'cookie',textAlign:'center',color:'rgb(13, 202, 240)'}}>길이 측정을 원하는 부위에 빨간 선을 놓으세요!</h3>
         </div>
         <div>
         <img src = "https://placeimg.com/300/300/4"
-            width='375' 
-            height='500'
-            style={{paddingRight: '50px',paddingLeft: '50px', paddingTop: '100px', paddingBottom: '100px',borderTopWidth: '0px'}}/>
+            style={{display: 'block', margin: '0px auto',paddingTop: '50px' ,paddingBottom: '50px'}}/>
             <h3 style={{fontFamily: 'cookie',textAlign:'center',color:'rgb(13, 202, 240)'}}>측정 완료!</h3>
         </div>
         </Slider>
--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
             bg={variant.toLowerCase()}
             key={idx}
             text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
-            style={{ width: '375px' ,height: '800px' }}
+            style={{ width: '100vw' ,height: '800px' }}
             className="mb-2"
             >
             
@@ -25,7 +25,7 @@ function Home() {
                 style={{fontSize: '3rem', marginTop: '15rem',textAlign:'center',fontFamily: 'cookie'}}>💍Ring-Fit💍</Card.Title>
                 <Card.Text>
                 <Link to ='/guide'><Button variant="outline-info"
-                style={{ color: 'white', borderColor: 'rgb(13, 202, 240)',borderTopWidth: '298px',paddingRight: '100px',paddingLeft: '100px',borderLeftWidth: '38px',borderRightWidth: '34px',borderBottomWidth: '137px',fontFamily: 'cookie',textAlign:'center'}}>시작하기!</Button>{' '}</Link>
+                style={{ color: 'white', borderColor: 'rgb(13, 202, 240)',borderTopWidth: '298px',paddingRight: '100px',paddingLeft: '100px',borderLeftWidth: '38px',borderRightWidth: '34px',borderBottomWidth: '137px',fontFamily: 'cookie',textAlign:'center'}}>시작하기🖐️</Button>{' '}</Link>
                 </Card.Text>
             </Card.Body>
             </Card>
