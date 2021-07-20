e7b6dc4c64b7dd67caac3431e0ff2b5b4f8391ef
jueun <rrnarg@gmail.com>
2021-07-20 14:41:40 +0900

    IMPROVE: Upload.js  수정

--- react/src/pages/Upload.js
+++ react/src/pages/Upload.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import Resizer from "react-image-file-resizer";
 import tempPreview from "../images/tempPreview.png";
 
