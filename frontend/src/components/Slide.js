1c2bbc8aab1a739ffe044f2ab02c1ec26c69b493
jueun <rrnarg@gmail.com>
2021-07-22 15:39:49 +0900

    IMPROVE: css 리팩터링

--- frontend/src/components/Slide.js
+++ frontend/src/components/Slide.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import { Button } from "react-bootstrap";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import guideImg1 from "../images/guide1.jpg";
@@ -21,158 +20,43 @@ export default class LazyLoad extends Component {
     };
     return (
       <div>
-        <div
-          style={{
-            textAlign: "center",
-            fontFamily: "ariblk",
-            marginTop: "45px",
-            width: "100vw",
-            fontSize: "20px",
-          }}
-        >
-          RINGFIT GUIDE
-        </div>
-
         <Slider {...settings}>
           <div>
-            <img
-              src={guideImg1}
-              style={{
-                display: "block",
-                margin: "0px auto",
-                paddingTop: "50px",
-                paddingBottom: "15px",
-                width: "300px",
-                height: "470px",
-              }}
-            />
-            <div
-              style={{
-                fontFamily: "ariblk",
-                fontSize: "21.3px",
-                paddingLeft: "50px",
-              }}
-            >
-              1&nbsp;.
-            </div>
-            <br></br>
-            <div style={{ textAlign: "center", paddingBottom: "33px" }}>
-              <div
-                style={{
-                  fontFamily: "OpenSans-Regular",
-                  textAlign: "center",
-                  width: "100vw",
-                  fontSize: "14px",
-                }}
-              >
+            <img className="guideImg" src={guideImg1} />
+            <div className="guideNum">1&nbsp;.</div>
+            <div style={{ textAlign: "center", paddingBottom: "1em" }}>
+              <div className="guideText">
                 prepare a coin and clean background.<br></br>The coin must be
                 100 won.
               </div>
             </div>
           </div>
           <div>
-            <img
-              src={guideImg2}
-              style={{
-                display: "block",
-                margin: "0px auto",
-                paddingTop: "50px",
-                paddingBottom: "15px",
-                width: "300px",
-                height: "470px",
-              }}
-            />
-            <div
-              style={{
-                fontFamily: "ariblk",
-                fontSize: "21.3px",
-                paddingLeft: "50px",
-              }}
-            >
-              2&nbsp;.
-            </div>
-            <br></br>
-            <div style={{ textAlign: "center", paddingBottom: "33px" }}>
-              <div
-                style={{
-                  fontFamily: "OpenSans-Regular",
-                  textAlign: "center",
-                  width: "100vw",
-                  fontSize: "14px",
-                }}
-              >
+            <img className="guideImg" src={guideImg2} />
+            <div className="guideNum">2&nbsp;.</div>
+            <div style={{ textAlign: "center", paddingBottom: "1em" }}>
+              <div className="guideText">
                 Put a coin on the left side of your<br></br>finger and upload a
                 picture.
               </div>
             </div>
           </div>
           <div>
-            <img
-              src={guideImg3}
-              style={{
-                display: "block",
-                margin: "0px auto",
-                paddingTop: "50px",
-                paddingBottom: "15px",
-                width: "300px",
-                height: "470px",
-              }}
-            />
-            <div
-              style={{
-                fontFamily: "ariblk",
-                fontSize: "21.3px",
-                paddingLeft: "50px",
-              }}
-            >
-              3&nbsp;.
-            </div>
-            <br></br>
-            <div style={{ textAlign: "center", paddingBottom: "33px" }}>
-              <div
-                style={{
-                  fontFamily: "OpenSans-Regular",
-                  textAlign: "center",
-                  width: "100vw",
-                  fontSize: "14px",
-                }}
-              >
-                Move the <span style={{color:"red"}}>red line</span> <br></br>to the desired ring position.
+            <img className="guideImg" src={guideImg3} />
+            <div className="guideNum">3&nbsp;.</div>
+            <div style={{ textAlign: "center", paddingBottom: "1em" }}>
+              <div className="guideText">
+                Move the <span style={{ color: "red" }}>red line</span>{" "}
+                <br></br>to the desired ring position.
               </div>
             </div>
           </div>
           <div>
-            <img
-              src={guideImg4}
-              style={{
-                display: "block",
-                margin: "0px auto",
-                paddingTop: "50px",
-                paddingBottom: "15px",
-                width: "300px",
-                height: "470px",
-              }}
-            />
-            <div
-              style={{
-                fontFamily: "ariblk",
-                fontSize: "21.3px",
-                paddingLeft: "50px",
-              }}
-            >
-              4&nbsp;.
-            </div>
-            <br></br>
-            <div style={{ textAlign: "center", paddingBottom: "33px" }}>
-              <div
-                style={{
-                  fontFamily: "OpenSans-Regular",
-                  textAlign: "center",
-                  width: "100vw",
-                  fontSize: "14px",
-                }}
-              >
-               Then RINGFIT will tell your ring FIT size.
+            <img className="guideImg" src={guideImg4} />
+            <div className="guideNum">4&nbsp;.</div>
+            <div style={{ textAlign: "center", paddingBottom: "1em" }}>
+              <div className="guideText">
+                Then RINGFIT will tell your ring FIT size.
               </div>
             </div>
           </div>
