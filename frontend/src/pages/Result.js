758da2eb7b78a687b3d0bce9c8ccfa339661bf2a
jueun <rrnarg@gmail.com>
2021-07-22 15:48:26 +0900

    IMPROVE: Result.js 주석 제거

--- frontend/src/pages/Result.js
+++ frontend/src/pages/Result.js
@@ -63,45 +63,5 @@ function Result() {
 export default Result;
 
 
-// 아래 해당 주석은 Result 페이지의 css 입히기 전 코드입니다.
-
-// import axios from "axios";
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-
-// function Result() {
-//   const [user, setUser] = useState({round: 0, size: 0});
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     const fetchUser = async() => {
-//       try {
-//         setLoading(true);
-//         const url = "http://localhost:5000/api/result";
-//         const response =  await axios.get(url);
-//         setUser({round: response.data.cirumference, size: response.data.size});
-//       }catch(e) {
-//         console.log(e);
-//       };
-//       setLoading(false);
-//     }
-//     fetchUser();
-//   },[]);
-
-//   if (loading) return <div>loading...</div>;
-//   return (
-//     <div>
-//       <div>
-//         <div>round {user.round}</div>
-//         <div>size {user.size}</div>
-//       </div>
-//       <Link to="/">home(temp)</Link>
-//     </div>
-//   );
-// }
-
-// export default Result;
-
-
 
 
