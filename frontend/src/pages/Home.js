1abd082b2648e9a358c1c040d708afe260181efe
jueun <rrnarg@gmail.com>
2021-07-22 16:40:53 +0900

    FIX: 홈 스크롤 문제 해결

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,38 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../css/homeImg.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-
-return(
-    <Link to ='/guide' style={{textDecoration: 'none'}} >
-        <div className="start-bg"
-        style={{ backgroundColor:'skyblue' ,height:'100vh' ,cursor: 'pointer'}} 
-    >
-
-        <h1  class="center" style={{color:'white',
-        textAlign:'center',
-        fontSize: '2.4rem',
-        fontFamily:'ariblk',
-        paddingTop: '318px',
-        marginTop: '0px',
-        marginBottom: '0px',
-        }}>RING FIT</h1>
-
-<div style={{ textAlign:'center',marginTop: '314px'}}>
-<h style={{
-textAlign:'center',
-fontFamily:'ariblk',
-color:'white',
-}}>touch the screen</h>
-</div>
-</div>
-</Link>
-)
-
+  return (
+    <Link to="/guide" style={{ textDecoration: "none" }}>
+      <div className="start-bg">
+        <div class="title">RING FIT</div>
+        <div className="titleText">touch the screen</div>
+      </div>
+    </Link>
+  );
 }
 
 export default Home;
-
-
