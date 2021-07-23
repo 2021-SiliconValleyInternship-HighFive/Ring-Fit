bc8d49d6a1f05a9b459c426e93a556b8730f735a
jueun <rrnarg@gmail.com>
2021-07-23 22:26:33 +0900

    NEW: user 데이터 삭제 추가

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -7,21 +7,34 @@ function User() {
         /* indexedDB에서 user data 불러오기 */
         const store = getObjectStore(DB_STORE_NAME, "readonly");
         let req = store.getAll();
-
-
         req.onsuccess=(e)=>{
             setUsers(e.target.result);
         }
         req.onerror = (err) => {
-            console.log("getAllError: ",err);
+            console.log("DATABASE ERROR: ",err);
         }
     },[])
+
+    /* userdata 삭제 */
+    const onRemove = (id) => {
+        const store = getObjectStore(DB_STORE_NAME, "readwrite");
+        let req = store.delete(id);
+        req.onsuccess=(e)=>{
+            setUsers(users.filter(user => user.id !== id));
+        }
+        req.onerror = (err) => {
+            console.log("DATABASE ERROR: ",err);
+        }
+    }
 
     return(
         <div>            
             {users.map((user) => {
                 return(
-                  <div key={user.id}>num: {user.id} round: {user.ringround} size: {user.ringsize}</div>  
+                    <div>
+                  <p key={user.id}>{user.LorR} {user.finger} {user.position} round: {user.round}mm size: {user.size}</p>
+                  <button onClick={() => {onRemove(user.id)}}>DELETE</button>
+                  </div>
                 );
             })}
         </div>
