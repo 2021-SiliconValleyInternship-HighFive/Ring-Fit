6c6666ea25586eb0bb65f8839bbc5e0443cd3478
jueun <rrnarg@gmail.com>
2021-07-23 12:36:42 +0900

    NEW: user data 불러오기 추가

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -4,29 +4,23 @@ function User() {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
+        /* indexedDB에서 user data 불러오기 */
         const store = getObjectStore(DB_STORE_NAME, "readonly");
-        let req = store.openCursor();
-        req.onsuccess= (e) => {
-            let cursor = e.target.result;
-            if(cursor){
-                console.log(cursor.value);
-                cursor.continue();
-            }
+        let req = store.getAll();
+        req.onsuccess=(e)=>{
+            setUsers(e.target.result);
         }
         req.onerror = (err) => {
-            console.error(err);
+            console.log("getAllError: ",err);
         }
-
     },[])
     return(
-        <div>
-            testing...
-            
-            {/*users.map((user) => {
+        <div>            
+            {users.map((user) => {
                 return(
-                  <div key={user.id}>id: {user.id}</div>  
+                  <div key={user.id}>num: {user.id} round: {user.ringround} size: {user.ringsize}</div>  
                 );
-            })*/}
+            })}
         </div>
     );
 };
