68589ec8acabb7541b00a6b875cea444b3f283f1
jueun <rrnarg@gmail.com>
2021-07-23 10:46:24 +0900

    NEW: indexedDB 데이터 콘솔창 출력

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,10 +1,32 @@
 import React, { useState, useEffect} from 'react';
 
 function User() {
+    const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        const store = getObjectStore(DB_STORE_NAME, "readonly");
+        let req = store.openCursor();
+        req.onsuccess= (e) => {
+            let cursor = e.target.result;
+            if(cursor){
+                console.log(cursor.value);
+                cursor.continue();
+            }
+        }
+        req.onerror = (err) => {
+            console.error(err);
+        }
+
+    },[])
     return(
         <div>
-
+            testing...
+            
+            {/*users.map((user) => {
+                return(
+                  <div key={user.id}>id: {user.id}</div>  
+                );
+            })*/}
         </div>
     );
 };
